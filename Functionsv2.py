ea5c60561ca40cc58ecfeb92fd20f7eaa565a647
Jorge <100428594@alumnos.uc3m.es>
2025-03-31 15:40:42 +0200

    SEMI AND EXPLICIT DONE

--- Functionsv2.py
+++ Functionsv2.py
@@ -2,7 +2,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def semiExplicitEuler(dthetadt,dPthetadt, F0, dt, tf):
+def semiImplicitEuler(dthetadt,dPthetadt, F0, dt, tf):
 
     t = np.arange(0, tf + dt, dt) # Numerical grid
     theta = np.zeros(len(t))
@@ -15,7 +15,21 @@ def semiExplicitEuler(dthetadt,dPthetadt, F0, dt, tf):
 
     return Ptheta, theta, t
 
-def ImplicitEuler(F, F0, dt, tf):
+def ExplicitEuler(dthetadt,dPthetadt, F0, dt, tf):
+
+    t = np.arange(0, tf + dt, dt) # Numerical grid    
+    theta = np.zeros(len(t))
+    Ptheta = np.zeros(len(t))
+    theta[0] = F0[0]
+    Ptheta[0] = F0[1]
+
+    for i in range(1,len(t)):
+        theta[i] = theta[i-1] + dt*dthetadt(t[i-1],Ptheta[i-1])
+        Ptheta[i] = Ptheta[i-1] + dt*dPthetadt(t[i-1],theta[i-1])
+
+    return Ptheta, theta, t
+
+def implicitEuler(dthetadt,dPthetadt, F0, dt, tf):
 
     t = np.arange(0, tf + dt, dt) # Numerical grid
     S = np.zeros(len(t))
@@ -26,32 +40,39 @@ def ImplicitEuler(F, F0, dt, tf):
 
     return t, S
     
-# Define parameters
-f = lambda t, s: s+ np.exp(-t) # ODE
-dt = 0.01 # Step size
-t = 1
-s0 = -1 # Initial Condition
-
 
 def hamiltonian():
     omega = 9.80665 # Frequency m/s^2
 
     # Hamiltonian equations
     dthetadt = lambda t, Ptheta: Ptheta
-    dPhetadt = lambda t, theta: -np.sin(theta)*omega 
-    return dthetadt, dPhetadt
+    dPthetadt = lambda t, theta: -np.sin(theta)*omega 
+    return dthetadt, dPthetadt
 
 F0 = np.array([np.pi/4, 0]) # Initial conditions
 dt = 0.01 # Step size
-t = 100 # Final time
+t = 20 # Final time
 
 dthetadt,dPhetadt = hamiltonian()
-Ptheta, theta, t = semiExplicitEuler(dthetadt, dPhetadt, F0, dt, t)
-
+Ptheta_exp, theta_exp, t_exp = ExplicitEuler(dthetadt, dPhetadt, F0, dt, t)
+Ptheta_sem, theta_sem, t_sem = semiImplicitEuler(dthetadt, dPhetadt, F0, dt, t)
 # t, sol = ExplicitEuler(f, s0, dt, t)
 
 # print()
-plt.plot(Ptheta, theta)
+plt.figure()
+
+plt.plot(Ptheta_exp, theta_exp)
+plt.plot(Ptheta_sem, theta_sem)
+plt.title('Phase Space')
+plt.xlabel('Ptheta')
+plt.ylabel('theta')
+plt.legend(['Explicit Euler', 'Semi-Implicit Euler'])
+plt.grid()
+plt.xlim([-5,5])
+plt.ylim([-2,2])
+plt.axhline(0, color='black', lw=0.5, ls='--')
+plt.axvline(0, color='black', lw=0.5, ls='--')
+plt.tight_layout()
 plt.show()
 
 

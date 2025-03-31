2e5f8d82a70b9edf8949fca043d9e3837ee6d307
aadamgb <154905832+aadamgb@users.noreply.github.com>
2025-03-31 10:48:42 +0200

    Explicit and Implicit first version

--- Functions.py
+++ Functions.py
@@ -1,5 +1,41 @@
 
-def ExplicitEuler():
-    
+import numpy as np
+import matplotlib.pyplot as plt
 
-def ImplicitEuler():
+def ExplicitEuler(F, F0, dt, tf):
+
+    t = np.arange(0, tf + dt, dt) # Numerical grid
+    S = np.zeros(len(t))
+    S[0] = F0
+
+    for i in range(1,len(t)):
+        S[i] = S[i-1] + dt*F(S[i-1],t[i-1])
+
+    return t, S
+
+def ImplicitEuler(F, F0, dt, tf):
+
+    t = np.arange(0, tf + dt, dt) # Numerical grid
+    S = np.zeros(len(t))
+    S[0] = F0
+
+    for i in range(1,len(t)):
+        S[i] = S[i-1] + dt*F(S[i],t[i])
+
+    return t, S
+    
+# Define parameters
+f = lambda t, s: s+ np.exp(-t) # ODE
+dt = 0.1 # Step size
+t = 1
+s0 = -1 # Initial Condition
+
+
+t, sol = ExplicitEuler(f, s0, dt, t)
+
+print(sol)
+plt.plot(t, sol)
+plt.show()
+
+
+# def ImplicitEuler():

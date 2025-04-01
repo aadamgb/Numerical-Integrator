dc0977cf6328fe500af50bc6f4a64f7cda00a1a8
Luuk Wintes <luukwin@planet.nl>
2025-04-01 02:29:28 +0200

    Change interface elements according to input necessary Improve documentation

--- Interface.py
+++ Interface.py
@@ -1,12 +1,19 @@
 import argparse
+import FunctionV3 as Function
 
 # Initialize argument parser
 parser = argparse.ArgumentParser()
 
-# Use `add_argument` with `action="store_true"` for boolean flags
+# Add all arguments as options for input
+# -- means it is a optional input, but since they are used we need default options
+# Type specifies the type of the input (int, float, string, etc.)
+# Help is what is displayed if using the help command
+# Choices are possible inputs, throws error if input differs
+
 parser.add_argument("-m", "--method", help="Call Euler method. Default method is implicit", choices=["I", "E", "i", "e"], default = "I")
-parser.add_argument("dT", type=int, help="Number of steps")
-parser.add_argument("IG", type=int, help="Initial guess")
+parser.add_argument("dT", type=float, help="Timestep")
+parser.add_argument("-IG1", "--initial_guess1", type=float, help="First initial gues", default = 0)
+parser.add_argument("-IG2", "--initial_guess2", type=float, help="Second initial gues", default = 0)
 parser.add_argument("FT", type=int, help="Final time")
 
 # Parse arguments
@@ -14,6 +21,8 @@ args = parser.parse_args()
 
 # Check which flag was used
 if args.method.upper() == "E":
-    print("Explicit", args.dT, args.IG, args.FT)
+    print("Explicit method is used")
+    Function.explicit_euler(args.initial_guess1, args.initial_guess2, int(args.FT / args.dT), args.dT)
 else:
-    print("Implicit", args.dT, args.IG, args.FT)
+    print("Implicit method is used")
+    Function.implicit_euler(args.initial_guess1, args.initial_guess2, int(args.FT / args.dT), args.dT)
